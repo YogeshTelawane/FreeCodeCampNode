acdb91aa3a13328ac6aee9b958051668a8a0e84e
Yogesh <yogeshtelawane8866@gmail.com>
2022-02-16 17:22:25 +0530

    Os file added

---
+++ os.js
@@ -0,0 +1,8 @@
+const os = require('os');
+
+// console.log(os.userInfo())
+// console.log(os.freemem())
+console.log(os.platform())
+console.log(os.version())
+console.log(os.uptime())
+console.log(os.cpus())
