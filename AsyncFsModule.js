5ac491731dfe8f66bf798f55f001d322225e6d8c
Yogesh <yogeshtelawane8866@gmail.com>
2022-02-16 20:51:13 +0530

    File system module added!

--- AsyncFsModule.js
+++ AsyncFsModule.js
@@ -2,6 +2,8 @@ const { readFile, writeFile} = require('fs');
 
 //Asynchronious way
 
+console.log('task 1');
+
 readFile('./content/demo.txt','utf8', (err, result)=>{
     if(err){
         console.log(err);
@@ -12,6 +14,9 @@ readFile('./content/demo.txt','utf8', (err, result)=>{
         console.log(result);
     }
 })
+
+console.log('task 2');
+console.log('task 3');
 
 writeFile('./content/demo2.txt',"This is writeFile method of fs module", (err, result)=>{
     if(err){
--- fsModule.js
+++ fsModule.js
@@ -2,11 +2,15 @@
 
 const {readFileSync, writeFileSync, readFile, writeFile} = require('fs');
 
+console.log('task 1');
+
 const read = readFileSync('./content/demo.txt', 'utf8');
 
 console.log(read);
+console.log('task 2');
+console.log('task 3');
 
-const write = writeFileSync('./content/demo.txt', "Hello from yogesh")
+//const write = writeFileSync('./content/demo.txt', "Hello from yogesh")
 
 
 
